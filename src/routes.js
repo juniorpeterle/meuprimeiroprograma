946d01d184a32c8b68116334309ec2331a262bf7
MoacirJr <juniorpeterle@gmail.com>
2021-01-02 22:17:06 -0300

    gpio

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,10 @@ var express = require('express');
 var routes = express.Router();
 var mongoose = require('mongoose');
 var usuariosModel = mongoose.model('usuarios');
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var LED = new Gpio(2, 'out'); //use GPIO pin 4, and specify that it is output
+var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
+
 //Rotas
 routes.get('/sensor',function(req,res){
     res.send({
@@ -33,6 +37,27 @@ routes.post('/cadastro',async function(req,res){
     var payload = req.body;
     var result = await usuariosModel.create(payload);
     res.json(result);
+})
+
+function blinkLED() { //function to start blinking
+    if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+      LED.writeSync(1); //set pin state to 1 (turn LED on)
+    } else {
+      LED.writeSync(0); //set pin state to 0 (turn LED off)
+    }
+  }
+  
+  function endBlink() { //function to stop blinking
+    clearInterval(blinkInterval); // Stop blink intervals
+    LED.writeSync(0); // Turn LED off
+    LED.unexport(); // Unexport GPIO to free resources
+  }
+  
+routes.post('/led', function(req,res){
+    Blinkled();
+    endBlink();
+    setTimeout(endBlink, 2000); //stop blinking after 5 seconds
+
 })
 
 module.exports = routes;
